e8d4d51167235245a21dc1cfc9fa599ca39b04dd
Jetup13 <62556437+Jetup13@users.noreply.github.com>
2021-02-06 04:38:28 -0800

    Update ArkOS Freeplay Theme Update.sh

--- ArkOS Freeplay Theme Update.sh
+++ ArkOS Freeplay Theme Update.sh
@@ -4,10 +4,11 @@
 #   Replace the REPURL by the correct one for your theme.
 
 REPURL="https://github.com/Jetup13/es-theme-freeplay/archive/master.zip"
+#extract the branch name from the provided URL
 REPBRANCH=$(grep -oP "[^\/]+(?=\.zip)" <<< "$REPURL")
+#extract the repository name from the provided URL
 REPNAME=$(grep -oP "[^\/]+(?=\/archive)" <<< "$REPURL")
 REPDIR=$REPNAME-$REPBRANCH
-BASEDIR=$(dirname $0)
 ABSPATH=$(readlink -f "$0")
 ABSDIR=$(dirname "$ABSPATH")
 cd "$ABSDIR"
@@ -20,6 +21,7 @@ if unzip -qt update.zip | grep 'No errors detected';then
     printf "\nUnzipping archive..."
     sudo unzip -qo update.zip -d ./temp/
     printf "\nOverwritting existing files..."
+    rm *.sh #removing previous scripts
     temppath=./temp/$REPDIR/*
     cp -rf $temppath ./
     printf "\nDeleting temporary files..."
